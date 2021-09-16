09707e14d5f65aef35fe3919614887c11a349248
Muammarbanana <muammarfanani2@gmail.com>
2021-09-16 10:30:49 +0700

    add gridview and floating action button

--- lib/views/productpage.dart
+++ lib/views/productpage.dart
@@ -13,30 +13,96 @@ class _ProductPageState extends State<ProductPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: [
-              const Expanded(
-                child: Text(
-                  "Product List",
-                  style: TextStyle(fontSize: 20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: Text(
+                      "Daftar Produk",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Badge(
+                    badgeContent: const Text(
+                      '3',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    child: const Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black87,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.grey[200],
+                padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 12,
+                    mainAxisSpacing: 12,
+                    crossAxisCount: 2,
+                  ),
+                  itemCount: 300,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      color: Colors.amber,
+                      child: Center(child: Text('$index')),
+                    );
+                  },
                 ),
               ),
-              Badge(
-                badgeContent: const Text(
-                  '3',
-                  style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        elevation: 1,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Icon(Icons.list_rounded),
+                    Text(
+                      "Category",
+                      style: TextStyle(color: Colors.blue),
+                    )
+                  ],
                 ),
-                child: const Icon(
-                  Icons.shopping_cart,
-                  color: Colors.black87,
+              ),
+              const SizedBox(width: 12),
+              GestureDetector(
+                onTap: () {},
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Icon(Icons.filter_list_rounded),
+                    Text(
+                      "Filter",
+                      style: TextStyle(color: Colors.blue),
+                    )
+                  ],
                 ),
-              )
+              ),
             ],
           ),
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
---
+++ lib/views/producttile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductTile extends StatelessWidget {
+  const ProductTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
